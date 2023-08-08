f23fbaa532a1fefbc511797f736abf87e328a9c4
Fernando Tonon de Rossi <tonon.fernando@hotmail.com>
2023-08-08 13:10:00 -0400

    Update qtmesheditor.rb

--- Casks/qtmesheditor.rb
+++ Casks/qtmesheditor.rb
@@ -1,6 +1,6 @@
 cask 'qtmesheditor' do
-  version '1.7.2'
-  sha256 '42ea67a503efac05b8b6c827d6c381525cfb948d7cf263d2a1f528b2c9e3b261'
+  version '1.8.0'
+  sha256 '41aad2917870b377894d0766d49ed5e7c39d31db3a40b9ffd5035bc9b5d754e8'
 
   url "https://github.com/fernandotonon/QtMeshEditor/releases/download/#{version}/QtMeshEditor-#{version}-MacOS.dmg"
   name 'QtMeshEditor'
